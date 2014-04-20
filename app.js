ec1632bfd4631d55bbbc435e13dd4012f3c7d0c4
canu johann <canu.johannl@gmail.com>
2014-04-20 15:40:58 +0900

    let heroku fix the port number

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.configure('production', function(){
 // Routes
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 4000 , function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
